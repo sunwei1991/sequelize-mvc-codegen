1230b830db780540db1c282b45b5fbbe022a7369
sunwei <1490479246@qq.com>
2017-03-28 16:48:05 +0800

    1、添加读取mysql表结构的功能；2、添加生成entity文件的功能

---
+++ config.js
@@ -0,0 +1,14 @@
+/**
+ * @fileOverview config
+ * @author sunwei on 2017-3-28
+ * @module config
+ */
+"use strict";
+module.exports={
+    dialect:'mysql',
+    host:'192.168.100.216',
+    port:33061,
+    user:'root',
+    password:'xhgc-!@#123',
+    database:'xhgc_teaching'
+};
--- start.js
+++ start.js
@@ -4,3 +4,9 @@
  * @module start
  */
 "use strict";
+global.smc = global.smc || {};
+smc.config = require('./config');
+smc.tools = require('./lib/tools');
+
+const codegen = require('./lib');
+new codegen().main();
