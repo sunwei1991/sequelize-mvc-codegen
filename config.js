8d355a81acd7acbcd3b7d2af0ff26068969b7d1c
sunwei <1490479246@qq.com>
2017-03-30 16:12:20 +0800

    修改配置

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports={
     dialect:'mysql',
     host:'192.168.100.216',
     port:33061,
-    user:'root',
-    password:'xhgc-!@#123',
-    database:'xhgc_teaching'
+    user:'dev',
+    password:'dev123',
+    database:'sg'
 };
